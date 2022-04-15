e54bf148baed8756e3f782ff02d8f0d152239969
Andrew <andrewtclarkson@gmail.com>
2022-04-14 20:10:16 -0400

    added empty response to address timeout

--- server/index.js
+++ server/index.js
@@ -238,7 +238,7 @@ app.get("/loggedin", loggedIn, (req, res, next) => {
   if (req.isAuthenticated()) {
     res.send(req.user);
   } else {
-    console.log("Not logged in");
+    res.send({});
   }
   //needs a response if not logged in?
 });
