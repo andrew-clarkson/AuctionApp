b560c3566471797f3b31e8c731cd7b521219d150
andrew <andrewtclarkson@gmail.com>
2022-06-13 16:49:37 -0400

    move end date to july

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
-import AuctionItem from "./AuctionItem";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import React, { useState, useEffect } from 'react';
+import AuctionItem from './AuctionItem';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 const App = () => {
   //
-  const closeDate = new Date(2022, 5, 15, 14, 0, 0);
+  const closeDate = new Date(2022, 6, 15, 14, 0, 0);
   //
 
   const [items, setItems] = useState([]);
@@ -13,14 +13,14 @@ const App = () => {
   console.log(loggedIn);
 
   const getAll = () => {
-    fetch("/all")
+    fetch('/all')
       .then((res) => res.json())
       .then((data) => setItems(data));
   };
 
   //candidate for useEffect?
   const isLoggedIn = () => {
-    fetch("/loggedin")
+    fetch('/loggedin')
       .then((res) => res.json())
       .then((data) => setLoggedIn(data));
   };
@@ -65,33 +65,33 @@ const App = () => {
       highBidderID: loggedIn.id,
     };
 
-    fetch("/bid", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+    fetch('/bid', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(updatedBid),
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log("Success:", data);
+        console.log('Success:', data);
         getAll(); //this makes it refresh immediately - ok tho?
       })
       .catch((error) => {
-        console.log("Error:", error);
+        console.log('Error:', error);
       });
   };
 
   const deleteItem = (id) => {
-    fetch("/delete", {
-      method: "POST",
-      headers: { "Content-Type": "text/plain" },
+    fetch('/delete', {
+      method: 'POST',
+      headers: { 'Content-Type': 'text/plain' },
       body: id,
     })
       .then((response) => response)
       .then((data) => {
-        console.log("Success:", data);
+        console.log('Success:', data);
       })
       .catch((error) => {
-        console.log("Error:", error);
+        console.log('Error:', error);
       });
     getAll();
   };
@@ -99,21 +99,21 @@ const App = () => {
   return (
     <div>
       <Navbar user={loggedIn} />
-      <div className="container ">
-        <div className="container">
-          <h4 className="text-center">Auction Close begins at:</h4>
-          <h5 className="text-center">
-            {closeDate.toLocaleString("en-US", {
-              day: "numeric",
-              month: "long",
-              year: "numeric",
-              hour: "numeric",
-              minute: "2-digit",
+      <div className='container '>
+        <div className='container'>
+          <h4 className='text-center'>Auction Close begins at:</h4>
+          <h5 className='text-center'>
+            {closeDate.toLocaleString('en-US', {
+              day: 'numeric',
+              month: 'long',
+              year: 'numeric',
+              hour: 'numeric',
+              minute: '2-digit',
             })}
           </h5>
         </div>
 
-        <div className="row ">
+        <div className='row '>
           {items
             .map((item, index) => (
               <AuctionItem
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-// // Importing the Bootstrap CSS
-// import "bootstrap/dist/css/bootstrap.min.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
